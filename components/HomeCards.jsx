919388267fe26aaf2ad360f341bde9dec3c9393b
MohanadddAmrr <154283829+MohanadddAmrr@users.noreply.github.com>
2024-05-08 23:47:51 +0300

    Create HomeCards.jsx

---
+++ components/HomeCards.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from './Card';
+
+const HomeCards = () => {
+  return (
+    <section className="py-4">
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HomeCards;
