c810fa64d58dcb5df54fb63b33c550efc5e6fd3c
zeina hossam <zeina.hossamfathy@gmail.com>
2024-05-09 01:24:55 +0300

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,44 @@
+const menubtn = document.querySelector("#menu-btn"); 
+const navlinks = document.querySelector(".navlinks");
+const menubtnicon = menubtn.querySelector("i");
+
+menubtn.addEventListener("click", (e) => {
+    navlinks.classList.toggle("open");
+  
+    const isOpen= navlinks.classList.contains("open");
+    menubtnicon.setAttribute("class",isOpen ? "ri-close-line":"ri-menu-line");
+});
+
+navlinks.addEventListener("click", (e)=> {
+    navlinks.classList.remove("open");
+    menubtnicon.setAttribute("class","ri-menu-line");
+});
+
+//for videos
+const btns= document.querySelectorAll(".nav-btn");
+const slides= document.querySelectorAll(".video-slide");
+const contents= document.querySelectorAll(".content");
+
+var slidernav = function(manual){
+  btns.forEach((btn) =>{
+    btn.classList.remove("active");
+  })
+  slides.forEach((slide) =>{
+    slide.classList.remove("active");
+  })
+  contents.forEach((content) =>{
+    content.classList.remove("active");
+  })
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+
+}
+
+btns.forEach((btn,i) => {
+    btn.addEventListener("click",()=> {
+        slidernav(i);
+    })
+})
+
