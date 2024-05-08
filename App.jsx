517a2d5011deccdb8d6c90f013312dc8bffbef10
MohanadddAmrr <154283829+MohanadddAmrr@users.noreply.github.com>
2024-05-08 23:04:26 +0300

    Workouts Page

---
+++ App.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import ExercisesPage from './pages/ExercisesPage';
+import NotFoundPage from './pages/NotFoundPage';
+import ExercisePage, { exerciseLoader } from './pages/ExercisePage';
+import AddExercisePage from './pages/AddExercisePage';
+import EditExercisePage from './pages/EditExercisePage';
+//import ExerciseListing from './ExerciseListing';
+//import AddedExercisesPage from './AddedExercisesPage';
+
+const App = () => {
+  /*add new exercise*/
+  const addExercise = async (newExercise) => {
+    const res = await fetch('/api/exercises', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newExercise),
+    });
+    return;
+  }
+
+
+}
+
+
+
+return (
+  <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={<MainLayout />}
+      >
+        <Route index element={<HomePage />} />
+        <Route path="/exercises" element={<ExercisesPage />} />
+        <Route path="/add-exercise" element={<AddExercisePage addExerciseSubmit={addExercise} />} />
+        <Route path="/edit-exercise/:id" element={<EditExercisePage updateExerciseSubmit={updateExercise} />} />
+        <Route path="/exercises/:id" element={<ExercisePage deleteExercise={deleteExercise} />} loader={exerciseLoader} />
+        <Route path="*" element={<NotFoundPage />} />
+
+      </Route>
+    </Routes>
+  </Router>
+);
+}
+
+export default App;
+
+
